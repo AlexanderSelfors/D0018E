c4c229cea07b9b9d8bd6e836e05c9ab5d818c6d0
Daniel Källberg <Daniel.kallberg@hotmail.com>
2021-11-10 00:27:04 +0100

    Created Login and register page as well as updated style.css

---
+++ Website2.0/login.php
@@ -0,0 +1,17 @@
+<?php
+    include_once 'header.php';
+?>
+    <div class="box">
+        <div class="button-box">
+            <form class="login-input">
+                <input type="text" class="input-field" placeholder ="Username">
+                <input type="text" class="input-field" placeholder ="Password">
+                <input type="checkbox" class="checkbox"><span>Keep me logged in</span>>
+                <button type="submit" class="submit-login">Log in</button>
+            </form>
+        </div>
+    </div>
+
+    
+</body>
+</html>
---
+++ Website2.0/register.php
@@ -0,0 +1,27 @@
+<?php
+    include_once 'header.php'
+?>
+
+
+
+
+
+
+    <div class="box">
+        <div class="button-box">
+            <form class="register-input">
+                <input type="text" class="input-field" placeholder ="Email">
+                <input type="text" class="input-field" placeholder ="Username">
+                <input type="text" class="input-field" placeholder ="Password">
+                <input type="text" class="input-field" placeholder ="First Name">
+                <input type="text" class="input-field" placeholder ="Last Name">
+                <input type="text" class="input-field" placeholder ="Adress">
+                <input type="text" class="input-field" placeholder ="Phone number">
+                <button type="submit" class="submit-register">Register</button>
+            </form>
+        </div>
+    </div>
+
+    
+</body>
+</html>

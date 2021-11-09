d209855968e3a644908ef31422989aea130050d1
Alexander Selfors <alexander.selfors@hotmail.se>
2021-11-09 13:38:03 +0100

    Begining of Front end

--- Website2.0/index.php
+++ Website2.0/index.php
@@ -2,13 +2,10 @@
     function db_connect(){
         $connection = mysqli_connect('utbweb.its.ltu.se', '19931112', '19931112', 'db19931112');
         if(mysqli_connect_errno()){
-            echo "Fuck";
+            echo "Connection to database failed";
         }else{
-            echo "Finally!";
         }
-        return $connection;
-
-        
+        return $connection;   
     }
 
     db_connect();
@@ -20,9 +17,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Trash</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css%22%3E">
+    <title>ThrashStudents</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>It finally fucking works!</h1>
+    <div class='Menu'>
+        <div class='right-menu'>
+            <a href="index.html" class="menu-item">Home</a>
+        </div>
+        <div class="left-menu">
+            <a href='#' class="menu-item">Login</a>
+            <a href='#'class="menu-item">Register</a>
+            <a href='#' ><i class="fa fa-shopping-cart" id='cart'></i></a>
+        </div>
+    </div>
 </body>
 </html>

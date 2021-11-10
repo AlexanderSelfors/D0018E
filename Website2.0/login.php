f4096fa2cca072374599e8c5d299cbf680bbdcba
Daniel Källberg <Daniel.kallberg@hotmail.com>
2021-11-10 01:17:32 +0100

    Added repeat password and changed type for password from text to password

--- Website2.0/login.php
+++ Website2.0/login.php
@@ -5,7 +5,7 @@
         <div class="button-box">
             <form class="login-input">
                 <input type="text" class="input-field" placeholder ="Username">
-                <input type="text" class="input-field" placeholder ="Password">
+                <input type="password" class="input-field" placeholder ="Password">
                 <input type="checkbox" class="checkbox"><span>Keep me logged in</span>>
                 <button type="submit" class="submit-login">Log in</button>
             </form>
--- Website2.0/register.php
+++ Website2.0/register.php
@@ -10,14 +10,15 @@
     <div class="box">
         <div class="button-box">
             <form class="register-input">
-                <input type="text" class="input-field" placeholder ="Email">
-                <input type="text" class="input-field" placeholder ="Username">
-                <input type="text" class="input-field" placeholder ="Password">
-                <input type="text" class="input-field" placeholder ="First Name">
-                <input type="text" class="input-field" placeholder ="Last Name">
-                <input type="text" class="input-field" placeholder ="Adress">
-                <input type="text" class="input-field" placeholder ="Phone number">
-                <button type="submit" class="submit-register">Register</button>
+                <input type="text" name="email" class="input-field" placeholder ="Email">
+                <input type="text" name="name" class="input-field" placeholder ="Username">
+                <input type="password" name="password" class="input-field" placeholder ="Password">
+                <input type="password" name="passwordrepeat" class="input-field" placeholder ="Repeat password">
+                <input type="text" name="fname" class="input-field" placeholder ="First Name">
+                <input type="text" name="lname" class="input-field" placeholder ="Last Name">
+                <input type="text" name="adress" class="input-field" placeholder ="Adress">
+                <input type="text" name="pnumber" class="input-field" placeholder ="Phone number">
+                <button type="submit" name="submit" class="submit-register">Register</button>
             </form>
         </div>
     </div>

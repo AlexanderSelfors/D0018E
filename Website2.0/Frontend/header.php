ce5aefb5c456ef0528e66fccb2115227d808cb57
Daniel Källberg <Daniel.kallberg@hotmail.com>
2021-11-11 16:50:44 +0100

    New header

--- Website2.0/Frontend/header.php
+++ Website2.0/Frontend/header.php
@@ -9,13 +9,25 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class='Menu'>
-        <div class='left-menu'>
+    <div class='header'>
+        <div class ='inner-header'>
+            <div class="logo">
+                <a href="index.php"><h1>Trash<span>Students</span></h1></a>
+            </div>
+            <div class="right-menu">
+                <a href='cart.php' ><li><i class="fa fa-shopping-cart" id='cart'></li></i></a>
+                <a href='login.php' class="menu-item"><li>Login</li></a>
+                <a href='register.php'class="menu-item"><li>Register</li></a>
+        </div>
+       </div>
+    </div>
+
+
+        <!-- <div class='left-menu'>
             <a href="index.php" class="menu-item">Home</a>
         </div>
         <div class="right-menu">
             <a href='login.php' class="menu-item">Login</a>
             <a href='register.php'class="menu-item">Register</a>
             <a href='cart.php' ><i class="fa fa-shopping-cart" id='cart'></i></a>
-        </div>
-    </div>
+        </div> -->

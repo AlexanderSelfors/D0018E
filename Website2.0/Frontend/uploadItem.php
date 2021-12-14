b73db30364e795442c99cfcc1eb53d7558eb95b5
Alexander Selfors <alexander.selfors@hotmail.se>
2021-12-14 14:14:29 +0100

    kategorier i uppload

--- Website2.0/Frontend/uploadItem.php
+++ Website2.0/Frontend/uploadItem.php
@@ -1,6 +1,6 @@
 <?php
     include_once 'header.php';
-
+    include_once '../Backend/dbconn.php'
 ?>
 
 <div class="box">
@@ -9,6 +9,17 @@
             <input type="number" class="input-field" placeholder ="Price" name ="price" min=0>
             <input type="number" class="input-field" placeholder ="Quantity" name ="Quan" min=0>
             <input type="text" class="input-field" placeholder ="URL to picture" name ="pic">
+            <label for='cat'>Choose a category</label>
+            <select name="cat" id="cat">
+            <?php
+                $sql = "SELECT * FROM category";
+                $result = mysqli_query($connection, $sql);
+                while($row = mysqli_fetch_assoc($result)){
+                    $catName = $row['catName'];
+                    echo"<option value='$catName'>$catName</option>";
+                }
+            ?>
+            </select>
             <button type="submit" name="submit" class="submit-login">Upload</button>
         </form>
 </div>

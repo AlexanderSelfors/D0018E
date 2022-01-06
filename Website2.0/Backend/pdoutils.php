c011b667329ec1bb9531749f7dec67fcee30382f
Singhto69 <Timmerskold@hotmail.com>
2022-01-06 12:28:13 +0100

    Update pdoutils.php

--- Website2.0/Backend/pdoutils.php
+++ Website2.0/Backend/pdoutils.php
@@ -74,12 +74,13 @@ function addtocart(){
         die();
     }
     header("Location: index.php?addedItem");
+    }
 
 
-    function checkout(){
+function checkout(){
     // Create db object
     $db = connect();
-
+    
     // Assign variables for session , used by script to alter tables
     $sessionUserUID = $_SESSION["uid"];
 
@@ -91,14 +92,31 @@ function addtocart(){
         
         // 1. Fetch array , each row in cart that belongs to sessionuserid
 
+        $cartarray = returnArray($stmt ="SELECT * FROM cart WHERE cart_userID = '$sessionProductID'" , $db);
+        
         // 2. Insert new order into order table , use sessionuserid
 
-        // 3. Fetch the new orderID using MAX
+
+        $sth = $db->prepare($stmt =  "INSERT INTO `orders`(`order_UID`) VALUES ('$sessionUserUID')");
+        $sth->execute();
+        sleep(0.2);
+        $tempnewOrderID = returnArray($stmt=("SELECT MAX(orderID) FROM orderdetails"),$db)['orderID'];
 
         // 4. for each row in 1. , insert into orderdetails 
 
+        foreach ($cartarray as $row ){
+            $tempCartProdId = $cartarray['cart_productID'];
+            $tempCartName = $cartarray['productName'];
+            $tempCartPrice = $cartarray['productPrice'];
+            $tempCartStock = $cartarray['productStock'];
+            $sth = $db->prepare($stmt = "INSERT INTO `orderdetails`(`detail_orderID`, `detail_productID`, `detailName`, `detailPrice`, `detailStock`) VALUES ('$tempnewOrderID','$tempCartProdId','$tempCartName','$tempCartPrice','$tempCartPrice')");
+            $sth->execute();
+        }
+
         // 5. remove from cart.
-        
+        $sth = $db->prepare($stmt = " DELETE  FROM `cart` WHERE cart_userID = '$sessionUserUID' ");
+        $sth->execute();
+
         // Release lock , commit changes , terminate connection
         $sth = $db->prepare($stmt = "UNLOCK TABLES");
         $sth->execute();
@@ -114,8 +132,10 @@ function addtocart(){
     }
     header("Location: index.php?addedItem");
 
-
-    }
 }
+
+    
+
+
 
 ?>
